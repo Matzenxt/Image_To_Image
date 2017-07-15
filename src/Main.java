87a7aa7f755852caced11a85fcecaf44de52e4cb
Matzenxt <matthias.lodner@web.de>
2017-07-16 00:38:32 +0200

    First commit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
+import javax.imageio.ImageIO;
+import java.awt.*;
+
 /**
  * Created by Matthias on 15.07.2017.
  */
 public class Main {
     public static void main(String[] args) {
         //Just an empty project
+        Image image = ImageIO.read();
     }
 }
