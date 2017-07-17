886c856247133c49315dd180d9faac528f38c5bd
Matzenxt <matthias.lodner@web.de>
2017-07-17 21:41:39 +0200

    Added function

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,37 @@
 import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
 
 /**
  * Created by Matthias on 15.07.2017.
  */
 public class Main {
-    public static void main(String[] args) {
-        //Just an empty project
-        Image image = ImageIO.read();
+    public static void main(String[] args) throws Exception {
+        File fileforeground = new File("/C:/Users/Matthias/Desktop/Kreis.png");
+
+        File folder = new File("./ImageInput");
+        File[] listOfFilesBackground = folder.listFiles();
+
+        Image imageforeground = ImageIO.read(fileforeground);
+
+        BufferedImage bufferedImage = null;
+        Graphics graphics = null;
+
+        for(File file : listOfFilesBackground){
+            if(file.isFile()){
+                bufferedImage = ImageIO.read(file);
+
+                graphics = bufferedImage.getGraphics();
+
+                graphics.drawImage(imageforeground,500,0,null);
+                graphics.dispose();
+
+                ImageIO.write(bufferedImage, "png", new File("./ImageOutput/" + file.getName()));
+            }
+        }
+
+
+
     }
 }
